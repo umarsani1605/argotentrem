3260ae750e40005ef2bcbf8c4399e85c49339302
Umar Sani <umarsani361@gmail.com>
2024-01-28 22:01:02 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import "preline/preline";
+
+const app = Vue.createApp({})
+
+// app.use(router)
+
+app.mount('#app')
---
+++ src/router/index.ts
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from "../pages/Home.vue";
+
+import { type IStaticMethods } from "preline/preline";
+declare global {
+  interface Window {
+    HSStaticMethods: IStaticMethods;
+  }
+}
+
+const routes = [
+  { path: '/', component: Home },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+router.afterEach((to, from, failure) => {
+  if (!failure) {
+    setTimeout(() => {
+      window.HSStaticMethods.autoInit();
+    }, 100)
+  }
+});
+
+export default router;
